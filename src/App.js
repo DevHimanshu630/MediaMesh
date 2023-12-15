ac532af5cd11859d59374232fb1d1571bc5b5410
Himanshu singh <123050257+DevHimanshu630@users.noreply.github.com>
2023-12-15 12:49:28 +0530

    retail

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Components/Home';
 import Pricing from './Pages/Pricing';
 import About from './Pages/About';
+import Retail from './Pages/Retail';
 
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/pricing" element={<Pricing />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/pricing" element={<Pricing />} />
+    // </Routes>
+    <Retail/>
     
    
   );
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -54,7 +54,6 @@ function Home() {
     };
 
 
-
     return (
         <>
             <div class="context ">
