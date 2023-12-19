adff8b06cb189a3c2c6a40b45846e299fcdee324
Himanshu singh <123050257+DevHimanshu630@users.noreply.github.com>
2023-12-19 17:07:20 +0530

    learn tutorial

--- src/App.js
+++ src/App.js
@@ -11,23 +11,25 @@ import Corporate from './Pages/Corporate';
 import DigitalSignage from './Pages/DigitalSignage';
 import Dooh from './Pages/Dooh';
 import Services from './Pages/Services';
+import Learn from './Pages/Learn';
 
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/pricing" element={<Pricing />} />
-      <Route path="/aboutus" element={<About />} />
-      <Route path="/retail" element={<Retail />} />
-      <Route path="/talktosales" element={<TalktoSales />} />
-      <Route path="/healthcare" element={<HealthCare />} />
-      <Route path="/hospitality" element={<Hospitality />} />
-      <Route path="/corporate" element={<Corporate />} />
-      <Route path='/digital-signage-software' element={<DigitalSignage />} />
-      <Route path='/dooh' element={<Dooh />} />
-      <Route path='/services' element={<Services />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<Home />} />
+    //   <Route path="/pricing" element={<Pricing />} />
+    //   <Route path="/aboutus" element={<About />} />
+    //   <Route path="/retail" element={<Retail />} />
+    //   <Route path="/talktosales" element={<TalktoSales />} />
+    //   <Route path="/healthcare" element={<HealthCare />} />
+    //   <Route path="/hospitality" element={<Hospitality />} />
+    //   <Route path="/corporate" element={<Corporate />} />
+    //   <Route path='/digital-signage-software' element={<DigitalSignage />} />
+    //   <Route path='/dooh' element={<Dooh />} />
+    //   <Route path='/services' element={<Services />} />
+    // </Routes>
+    <Learn/>
 
 
   );
