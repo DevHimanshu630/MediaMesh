f7f9ace72cb212e392d0fc00681c1086d8545ce2
Arshadansa <officialarshad16@gmail.com>
2024-01-09 12:36:57 +0530

    fixing-height-problem-in-virtual-tour

--- src/Pages/new.jsx
+++ src/Pages/new.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function New() {
     return (
-
-        <iframe src="https://app.lapentor.com/sphere/office-temp" frameborder="0" width="100%" height="960px" scrolling="no" allow="vr,gyroscope,accelerometer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>)
+        <div className='h-[100vh]'>
+            <iframe src="https://app.lapentor.com/sphere/office-temp" frameborder="0" width="100%" height="100%" scrolling="no" allow="vr,gyroscope,accelerometer" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
+        </div>
+    )
 }
 
 export default New
