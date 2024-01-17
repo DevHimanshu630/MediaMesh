b6a06eff58b961240accd1257c4da33b6623c262
Arshadansa <officialarshad16@gmail.com>
2024-01-17 17:01:13 +0530

    updating-national-archives-logo

--- src/Components/ClientCarousel.jsx
+++ src/Components/ClientCarousel.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import client1 from '../images/ichrlogo.png'; // Example path
-import client2 from '../images/moe.png';
-import client3 from '../images/ngma-logo.png';
+import client3 from '../images/moe.png';
+import client2 from '../images/ngma-logo.png';
+import client4 from '../images/NationalArchiveslogo.png';
 import '../Css/Home.css'; // Replace with the correct path to your CSS file
 
 function ClientCarousel() {
     const clients = [
+        { id: 4, image: client4 },
         { id: 1, image: client1 },
-        { id: 3, image: client3 },
         { id: 2, image: client2 },
+        { id: 3, image: client3 },
 
 
     ];
 
     return (
-        <div className='w-full flex   mt-8 scroll-container'>
+        <div className='w-full flex  items-center  mt-8 scroll-container'>
             {clients.map((clientItem) => (
                 <img
                     key={clientItem.id}
